4f54de567d7785976b8a3bfb6f698fc6a7ca23c5
Артем Зимин <wade_92@mail.ru>
2023-01-25 17:37:35 +0300

    v1.1

--- backend/foodgram/entrypoint.sh
+++ backend/foodgram/entrypoint.sh
@@ -3,8 +3,8 @@
 echo "Apply database migrations"
 python manage.py migrate
 
-#echo "Load ingredients to database"
-#python manage.py load_data
+echo "Load ingredients to database"
+python manage.py load_data
 
 echo "Collect static files"
 python manage.py collectstatic --noinput
